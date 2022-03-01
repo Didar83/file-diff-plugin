34ad611dce8c2f3315d59a19755839f6fc1e129c
d.akulov <d.akulov@aix.kz>
2022-03-01 16:17:03 +0600

    gradle plugin

---
+++ src/main/groovy/com/didarakulov/filediff/FileDiffExtension.groovy
@@ -0,0 +1,8 @@
+package com.didarakulov.filediff
+
+import org.gradle.api.file.RegularFileProperty
+
+abstract class FileDiffExtension {
+    abstract RegularFileProperty getFile1()
+    abstract RegularFileProperty getFile2()
+}
